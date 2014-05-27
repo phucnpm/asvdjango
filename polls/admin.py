2aa5085f20f736f5b23c4c5dc111a0031152383c
shenlong3030 <shenlong3030@gmail.com>
2014-05-27 15:55:32 +0700

    init level 1, manage admin UI

--- polls/admin.py
+++ polls/admin.py
@@ -2,6 +2,25 @@ from django.contrib import admin
 from polls.models import Poll
 from polls.models import Choice
 
+# Class to define extension for admin UI of Polls CRUD
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
-admin.site.register(Poll)
+# Class to define admin UI of Polls CRUD
+class PollAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    # display Choice when CRUD a Poll using ChoiceInLine admin class
+    inlines = [ChoiceInline]
+    # display Polls as a list with 3 fields
+    list_display = ('question', 'pub_date', 'was_published_recently')
+    # display Poll filter control based on pub_date
+    list_filter = ['pub_date']
+    # display search field base on question
+    search_fields = ['question']
+
+admin.site.register(Poll, PollAdmin)
 admin.site.register(Choice)
